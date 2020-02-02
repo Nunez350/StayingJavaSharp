b90a65b6ad97ca93f0c3766ebfd95313c921ee2c
Roy Nunez <roynunez273@gmail.com>
2020-02-02 12:26:17 -0500

    comprator

---
+++ InterviewPrep/Comparator/AlToa.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class AlToa {
+  public static void main(String[] args) {
+    ArrayList<String> stringList = new ArrayList<>();
+
+    stringList.add("ASS");
+    stringList.add("Ba");
+    stringList.add("DaS");
+    stringList.add("AddS");
+    stringList.add("ASdd");
+
+    System.out.println("Before sort" + stringList);
+
+    Collections.sort(stringList, new SortComparator());
+
+  System.out.println("after sort" + stringList);
+}
+}
